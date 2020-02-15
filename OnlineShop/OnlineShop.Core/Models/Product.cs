9a95df2a9325e22ada03ee0bcd262197827f3152
PeterElGassar <PeterElGassar@users.noreply.github.com>
2020-02-15 04:33:06 +0200

    product Models And InMemory Product Repository Bulit

---
+++ OnlineShop/OnlineShop.Core/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace OnlineShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+
+        [StringLength(30, ErrorMessage = "The maximum length of Name is 30 Charachter")]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(0, 10000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+
+        /// <summary>
+        /// In this constructor i will Genrat Id  
+        /// </summary>
+        public Product()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ OnlineShop/OnlineShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using OnlineShop.Core;
+using OnlineShop.Core.Models;
+
+namespace OnlineShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            //first cast cache Object If can convert it to type  List<Product>
+            //if not products is null 
+            products = cache["products"] as List<Product>;
+
+
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            Product getProductToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (getProductToUpdate != null)
+            {
+                getProductToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+
+        }
+
+        public Product find(string id)
+        {
+            var getProduct = products.Find(p => p.Id == id);
+            if (getProduct != null)
+            {
+                return getProduct;
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+
+        public void Delete(string id)
+        {
+            Product getProductToDel = products.Find(p => p.Id == id);
+
+            if (getProductToDel != null)
+            {
+                products.Remove(getProductToDel);
+            }
+            else
+            {
+                throw new Exception("Product Not Found");
+            }
+        }
+
+
+    }
+}
