67c392b6daca3e36061ca96e9a81fcaa2317c93a
PeterElGassar <PeterElGassar@users.noreply.github.com>
2020-02-18 11:58:48 +0200

    Added Entity Framework

---
+++ OnlineShop/OnlineShop.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace OnlineShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OnlineShop.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OnlineShop.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
