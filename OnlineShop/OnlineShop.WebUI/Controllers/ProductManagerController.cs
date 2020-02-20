970277f595d875ba8f23c99cde0dc206597299b5
PeterElGassar <PeterElGassar@users.noreply.github.com>
2020-02-21 01:07:52 +0200

    Add Product Upload

--- OnlineShop/OnlineShop.WebUI/Controllers/ProductManagerController.cs
+++ OnlineShop/OnlineShop.WebUI/Controllers/ProductManagerController.cs
@@ -7,6 +7,10 @@ using OnlineShop.Core.Models;
 using OnlineShop.Core.ViewModels;
 using OnlineShop.DataAccess.InMemory;
 using OnlineShop.Core.Contracts;
+using System.IO;
+using System.Web.Helpers;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace OnlineShop.WebUI.Controllers
 {
@@ -20,7 +24,7 @@ namespace OnlineShop.WebUI.Controllers
             IRepository<ProductCategory> productCategoryContext)
         {
             context = productContext;
-            ProductCategoris = productCategoryContext;            
+            ProductCategoris = productCategoryContext;
         }
 
         // GET: ProductManager
@@ -40,19 +44,92 @@ namespace OnlineShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
                 return View(product);
             }
-            else
-            {
-                context.Insert(product);
-                context.Commit();
 
-                return RedirectToAction("Index");
+
+            #region Insert Image Of Product
+            string imageExtention = Path.GetExtension(file.FileName).ToLower();
+
+            string[] vaildTypes = { ".jpg", ".jpeg", ".png", ".gif" };
+
+            if (file != null && file.ContentLength > 0)
+            {
+                if (vaildTypes.Any(item => item == imageExtention))
+                {
+
+
+                    //Main Directory For All Products
+                    //Main Directory Must Exists
+                    var origenalDirectory = new DirectoryInfo(Server.MapPath(@"\") + @"Images\Uploads");
+
+                    //Create Some Folders To Every Single Product
+                    string productsPath = Path.Combine(origenalDirectory.ToString(), "Products");
+
+                    string singleProductPath = Path.Combine(origenalDirectory.ToString(), @"Products\" + product.Id);
+
+                    string productThumbPath = Path.Combine(origenalDirectory.ToString(), @"Products\" + product.Id + @"\Thumbs");
+
+                    string productGalleryPath = Path.Combine(origenalDirectory.ToString(), @"Products\" + product.Id + @"\Gallery");
+
+                    string productGalleryThumbsPath = Path.Combine(origenalDirectory.ToString(), @"Products\" + product.Id + @"\Gallery\Thumbs");
+                    //Exist All Path IF Not Exists yet
+                    if (!Directory.Exists(productsPath))
+                        Directory.CreateDirectory(productsPath);
+
+                    if (!Directory.Exists(singleProductPath))
+                        Directory.CreateDirectory(singleProductPath);
+
+                    if (!Directory.Exists(productThumbPath))
+                        Directory.CreateDirectory(productThumbPath);
+
+                    if (!Directory.Exists(productGalleryPath))
+                        Directory.CreateDirectory(productGalleryPath);
+
+                    if (!Directory.Exists(productGalleryThumbsPath))
+                        Directory.CreateDirectory(productGalleryThumbsPath);
+
+                    //======================================================//
+                    string imageName = file.FileName;
+                    product.Image = imageName;
+                    //======================Set Original Images ================================//
+                    string orignalImage = string.Format(@"{0}\{1}", singleProductPath, imageName);
+                    file.SaveAs(orignalImage);
+                    //======================Set Thumb Images ================================//
+                    string thumbIamge = string.Format(@"{0}\{1}", productThumbPath, imageName);
+                    WebImage img = new WebImage(file.InputStream);
+                    img.Resize(480, 640, true, true).Crop(1, 1, 1, 1).Write();
+                    img.Save(thumbIamge);
+
+                }
+
+                //thumbNail.Save(thumbIamge);
+                //using (Image img = Image.FromFile(orgFile))
+                //{
+                //    Image thumbNail = new Bitmap(width, height, img.PixelFormat);
+                //    Graphics g = Graphics.FromImage(thumbNail);
+                //    g.CompositingQuality = CompositingQuality.HighQuality;
+                //    g.SmoothingMode = SmoothingMode.HighQuality;
+                //    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                //    Rectangle rect = new Rectangle(0, 0, width, height);
+                //    g.DrawImage(img, rect);
+                //    thumbNail.Save(resizedFile, format);
+                //}
+
             }
+
+            #endregion
+
+            context.Insert(product);
+            context.Commit();
+
+            TempData["Message"] = "Product Add Successfully";
+            return RedirectToAction("Index");
+
 
         }
 
@@ -75,7 +152,7 @@ namespace OnlineShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string id)
+        public ActionResult Edit(Product product, string id, HttpPostedFileBase file)
         {
             //First Find Specific Item To Edit id
             Product productToEdit = context.Find(id);
@@ -91,15 +168,55 @@ namespace OnlineShop.WebUI.Controllers
                 {
                     return View(product);
                 }
+
                 productToEdit.Name = product.Name;
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
                 productToEdit.Price = product.Price;
-                productToEdit.Image = product.Image;
 
+                #region Update Image
+                if (file != null && file.ContentLength > 0)
+                {
+                    var orignalPath = new DirectoryInfo(Server.MapPath(@"\Images\Uploads"));
+
+                    string stringPath1 = Path.Combine(orignalPath.ToString() + "\\Products\\" + id.ToString());
+                    string StringPath2 = Path.Combine(orignalPath.ToString() + "\\Products\\" + id.ToString() + "\\Thumbs");
+
+                    DirectoryInfo direct1 = new DirectoryInfo(stringPath1);
+                    DirectoryInfo direct2 = new DirectoryInfo(StringPath2);
+
+                    //Get the content of the files and delete them all
+                    foreach (var file1 in direct1.GetFiles())
+                    {
+                        file1.Delete();
+                    }
+                    foreach (var file2 in direct2.GetFiles())
+                    {
+                        file2.Delete();
+                    }
+
+                    //New Image Data
+                    string imageName = file.FileName;
+                    productToEdit.Image = imageName;
+
+                    string path1 = string.Format(@"{0}\\{1}", stringPath1, imageName);
+                    file.SaveAs(path1);
+
+                    string path2 = string.Format(@"{0}\\{1}", StringPath2, imageName);
+                    WebImage img = new WebImage(file.InputStream);
+                    img.Resize(480, 640, true, true).Crop(1, 1, 1, 1).Write();
+                    img.Save(path2);
+                }
+
+                #endregion
                 context.Commit();
                 return RedirectToAction("Index");
+
             }
+
+
+
+
         }
 
 
---
+++ OnlineShop/OnlineShop.WebUI/Scripts/MainScript.js
@@ -0,0 +1,18 @@
+﻿
+var inputFile = document.getElementById("file");
+var ImagePreview = document.querySelector(".imagePreview");
+
+inputFile.addEventListener("change", function (e) {
+    debugger;
+    var $this = this;
+
+    if ($this.files && $this.files[0]) {
+
+        const reader = new FileReader();
+        reader.onload = function () {
+            ImagePreview.setAttribute("src",reader.result)
+        }
+        reader.readAsDataURL($this.files[0]);
+
+    }
+});
