bee6586c6a9714f284bf7840367a7d09f8202e9f
PeterElGassar <PeterElGassar@users.noreply.github.com>
2020-02-25 21:48:21 +0200

    Added Basket Models

--- OnlineShop/OnlineShop.DataAccess.SQL/DataContext.cs
+++ OnlineShop/OnlineShop.DataAccess.SQL/DataContext.cs
@@ -10,12 +10,13 @@ namespace OnlineShop.DataAccess.SQL
     public class DataContext : DbContext
     {
         public DataContext()
-            :base("DefaultConnection")
+            : base("DefaultConnection")
         {
 
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
-
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
