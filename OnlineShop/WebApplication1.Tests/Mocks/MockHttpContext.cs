a0396362a03184bba22d05fb72809c8b2ca2f42e
PeterElGassar <PeterElGassar@users.noreply.github.com>
2020-03-01 08:12:12 +0200

    Added Tests

---
+++ OnlineShop/WebApplication1.Tests/Mocks/MockHttpContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OnlineShop.WebUI.Tests.Mocks
+{
+    public class MockHttpContext : HttpContextBase
+    {
+        private MockResponse response;
+        private MockRequest request;
+        private HttpCookieCollection cookies;
+
+        public MockHttpContext()
+        {
+            cookies = new HttpCookieCollection();
+            response = new MockResponse(cookies);
+            request = new MockRequest(cookies);
+        }
+
+
+        //Override Response Property
+        public override HttpResponseBase Response
+        {
+            get
+            {
+                return response;
+            }
+        }
+
+        //Override Request Property
+        public override HttpRequestBase Request
+        {
+            get
+            {
+                return request;
+            }
+        }
+
+
+    }
+
+}
+/// <summary>
+/// 
+/// </summary>
+public class MockResponse : HttpResponseBase
+{
+    private readonly HttpCookieCollection cookies;
+
+    public MockResponse(HttpCookieCollection cookies)
+    {
+        this.cookies = cookies;
+    }
+
+    public override HttpCookieCollection Cookies
+    {
+        get
+        {
+            return cookies;
+        }
+    }
+
+}
+/// <summary>
+/// 
+/// </summary>
+public class MockRequest : HttpRequestBase
+{
+    private readonly HttpCookieCollection cookies;
+
+    public MockRequest(HttpCookieCollection cookies)
+    {
+        this.cookies = cookies;
+    }
+
+    public override HttpCookieCollection Cookies
+    {
+        get
+        {
+            return cookies;
+        }
+    }
+
+}
+
+
+
